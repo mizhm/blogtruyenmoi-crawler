e9e44b4e57388e5982a88816749bbfa82ab98dbc
mizhm <minhnt141204@gmail.com>
2024-10-06 00:54:56 +0700

    fix: replace by puppeteer-extra

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const xlsx = require('xlsx');
 
