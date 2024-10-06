41c22905701bcca1701b671947d02a76444c5470
mizhm <minhnt141204@gmail.com>
2024-10-06 21:06:12 +0700

    fix: attemp to rerun when timeout happen

--- index.js
+++ index.js
@@ -25,67 +25,71 @@ async function fetchMangaLinks(page) {
     });
     return links;
   });
-  // console.log(`Fetched ${mangaLinks.length} links on current page`);
   return mangaLinks;
 }
 
 async function fetchMangaLinksChunk(startPage, endPage) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for Puppeteer on GitHub Actions
-    timeout: 120000,
-    defaultViewport: null,
-  }); // Increase protocolTimeout to 120 seconds
-  const page = await browser.newPage();
-  await page.goto(baseUrl, {
-    waitUntil: 'networkidle2',
-    timeout: 60000,
-  });
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for Puppeteer on GitHub Actions
+      timeout: 120000,
+      defaultViewport: null,
+    }); // Increase protocolTimeout to 120 seconds
+    const page = await browser.newPage();
+    await page.goto(baseUrl, {
+      waitUntil: 'networkidle2',
+      timeout: 60000,
+    });
 
-  await page.evaluate((startPage) => {
-    window.LoadListMangaPage(startPage);
-  }, startPage);
+    await page.evaluate((startPage) => {
+      window.LoadListMangaPage(startPage);
+    }, startPage);
 
-  await page.waitForFunction(
-    `document.querySelector(".current_page").textContent === "${startPage}"`,
-    { timeout: 60000 },
-  );
+    await page.waitForFunction(
+      `document.querySelector(".current_page").textContent === "${startPage}"`,
+      { timeout: 60000 },
+    );
 
-  const allMangaLinks = [];
-  let currentPage = startPage;
-  let hasNextPage = true;
+    const allMangaLinks = [];
+    let currentPage = startPage;
+    let hasNextPage = true;
 
-  while (hasNextPage && currentPage <= endPage) {
-    try {
-      const mangaLinks = await fetchMangaLinks(page);
-      allMangaLinks.push(...mangaLinks);
+    while (hasNextPage && currentPage <= endPage) {
+      try {
+        const mangaLinks = await fetchMangaLinks(page);
+        allMangaLinks.push(...mangaLinks);
 
-      // Check if there is a next page button and click it
-      const nextPageButton = await page.$(
-        `span.page > a[href="javascript:LoadListMangaPage(${
-          currentPage + 1
-        })"]`,
-      );
-      if (nextPageButton) {
-        await nextPageButton.evaluate((button) => button.click());
-        await page.waitForFunction(
-          `document.querySelector(".current_page").textContent === "${
+        // Check if there is a next page button and click it
+        const nextPageButton = await page.$(
+          `span.page > a[href="javascript:LoadListMangaPage(${
             currentPage + 1
-          }"`,
-          { timeout: 60000 },
+          })"]`,
         );
-        currentPage++;
-      } else {
+        if (nextPageButton) {
+          await nextPageButton.evaluate((button) => button.click());
+          await page.waitForFunction(
+            `document.querySelector(".current_page").textContent === "${
+              currentPage + 1
+            }"`,
+            { timeout: 60000 },
+          );
+          currentPage++;
+        } else {
+          hasNextPage = false;
+        }
+      } catch (error) {
+        console.error(`Error on page ${currentPage}:`, error.message);
         hasNextPage = false;
       }
-    } catch (error) {
-      console.error(`Error on page ${currentPage}:`, error.message);
-      hasNextPage = false;
     }
-  }
 
-  await browser.close();
-  return allMangaLinks;
+    await browser.close();
+    return allMangaLinks;
+  } catch (error) {
+    fetchMangaLinksChunk(startPage, endPage);
+    console.log('Error:', error.message);
+  }
 }
 
 async function fetchAllMangaLinks() {
@@ -101,72 +105,83 @@ async function fetchAllMangaLinks() {
 }
 
 async function fetchMangaDetails(mangaLinks) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for Puppeteer on GitHub Actions
-    defaultViewport: null,
-    timeout: 120000,
-  });
-  const page = await browser.newPage();
-  const mangaDetails = [];
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for Puppeteer on GitHub Actions
+      defaultViewport: null,
+      timeout: 120000,
+    });
+    const page = await browser.newPage();
+    const mangaDetails = [];
 
-  for (const manga of mangaLinks) {
-    try {
-      await page.goto(manga.link, {
-        waitUntil: 'networkidle2',
-        timeout: 60000,
-      });
-      const details = await page.evaluate(() => {
-        const name = document.querySelector('h1').innerText.trim();
-        const author = Array.from(
-          document.querySelectorAll('a[href*="/tac-gia/"]'),
-        )
-          .map((el) => el.innerText.trim())
-          .join(', ');
-        const genre = Array.from(
-          document.querySelectorAll('.description a[href*="/theloai/"]'),
-        )
-          .map((el) => el.innerText.trim())
-          .join(', ');
-        const summary = document
-          .querySelector('.detail .content')
-          .innerText.trim();
+    for (const manga of mangaLinks) {
+      try {
+        await page.goto(manga.link, {
+          waitUntil: 'networkidle2',
+          timeout: 60000,
+        });
+        const details = await page.evaluate(() => {
+          const name = document.querySelector('h1').innerText.trim();
+          const author = Array.from(
+            document.querySelectorAll('a[href*="/tac-gia/"]'),
+          )
+            .map((el) => el.innerText.trim())
+            .join(', ');
+          const genre = Array.from(
+            document.querySelectorAll('.description a[href*="/theloai/"]'),
+          )
+            .map((el) => el.innerText.trim())
+            .join(', ');
+          const summary = document
+            .querySelector('.detail .content')
+            .innerText.trim();
 
-        const pageViews = document.querySelector('#PageViews').innerText.trim();
-        const likeCount = document.querySelector('#LikeCount').innerText.trim();
-        const spanColorRed = document.querySelectorAll(
-          '.description span.color-red',
+          const pageViews = document
+            .querySelector('#PageViews')
+            .innerText.trim();
+          const likeCount = document
+            .querySelector('#LikeCount')
+            .innerText.trim();
+          const spanColorRed = document.querySelectorAll(
+            '.description span.color-red',
+          );
+          const status =
+            spanColorRed.length != 0
+              ? spanColorRed[spanColorRed.length - 1].innerText.trim()
+              : spanColorRed[0].innerText.trim();
+          const anotherName =
+            spanColorRed.length > 1
+              ? Array.from(spanColorRed)
+                  .slice(0, -1)
+                  .map((span) => span.innerText.trim())
+                  .join(', ')
+              : 'Khong co ten khac';
+          return {
+            name,
+            author,
+            genre,
+            summary,
+            pageViews,
+            likeCount,
+            status,
+            anotherName,
+          };
+        });
+        mangaDetails.push({ ...manga, ...details });
+      } catch (error) {
+        console.error(
+          `Error fetching details for ${manga.link}:`,
+          error.message,
         );
-        const status =
-          spanColorRed.length != 0
-            ? spanColorRed[spanColorRed.length - 1].innerText.trim()
-            : spanColorRed[0].innerText.trim();
-        const anotherName =
-          spanColorRed.length > 1
-            ? Array.from(spanColorRed)
-                .slice(0, -1)
-                .map((span) => span.innerText.trim())
-                .join(', ')
-            : 'Khong co ten khac';
-        return {
-          name,
-          author,
-          genre,
-          summary,
-          pageViews,
-          likeCount,
-          status,
-          anotherName,
-        };
-      });
-      mangaDetails.push({ ...manga, ...details });
-    } catch (error) {
-      console.error(`Error fetching details for ${manga.link}:`, error.message);
+      }
     }
-  }
 
-  await browser.close();
-  return mangaDetails;
+    await browser.close();
+    return mangaDetails;
+  } catch (error) {
+    fetchMangaDetails(mangaLinks);
+  }
 }
 
 async function saveToExcel(mangaDetails) {
