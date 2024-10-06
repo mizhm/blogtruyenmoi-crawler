efdb2f42735a08ae3a2cbcd814ff95e7c7b5f939
mizhm <minhnt141204@gmail.com>
2024-10-06 23:45:09 +0700

    fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const requestTimeout = 10000; // 10 seconds
 const axiosInstance = axios.create({
   headers: {
     'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
   },
   timeout: requestTimeout,
 });
